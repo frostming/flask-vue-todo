e5330497bb0a5457778160aeff0082549214d06a
Frost Ming <mianghong@gmail.com>
2018-09-18 22:08:45 +0800

    fix a mini error

--- backend/settings.py
+++ backend/settings.py
@@ -4,6 +4,7 @@ here = os.path.dirname(__file__)
 
 SECRET_KEY = 'flask-vue-todo-app'
 WTF_CSRF_ENABLED = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 if 'DATABASE_URL' in os.environ:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
--- frontend/src/store/todos/actions.js
+++ frontend/src/store/todos/actions.js
@@ -14,37 +14,37 @@ export default {
     const vm = this
     return todoApi.addTodo(todo).then(data => {
       commit('addTodo', data.todo)
-    }).catch((e)=>{
+    }).catch(e => {
       vm.error(e.response.data.description)
     })
   },
   [types.REMOVE_TODO] ({commit}, todo) {
     commit('removeTodo', todo)
     const vm = this
-    return todoApi.removeTodo(todo).catch((e)=>{
+    return todoApi.removeTodo(todo).catch(e => {
       vm.error(e.response.data.description)
     })
   },
   [types.TOGGLE_TODO] ({commit}, todo) {
     commit('editTodo', {todo, done:!todo.done})
     const vm = this
-    return todoApi.editTodo(todo).catch((e)=>{
+    return todoApi.editTodo(todo).catch(e => {
       vm.error(e.response.data.description)
     })
   },
   [types.EDIT_TODO] ({commit}, {todo, value}) {
     commit('editTodo', {todo, text:value})
     const vm = this
-    return todoApi.editTodo(todo).catch((e)=>{
+    return todoApi.editTodo(todo).catch(e => {
       vm.error(e.response.data.description)
     })
   },
   [types.GET_TODOS] ({commit}) {
     const vm = this
     return todoApi.getTodos().then(data => {
-      commit('setTodos', data).catch((e)=>{
-        vm.error(e.response.data.description)
-      })
+      commit('setTodos', data)
+    }).catch(e => {
+      vm.error(e.response.data.description)
     })
   }
 }
